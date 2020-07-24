791f9e2af78ce8b91cabe4246cd27a2ec3691742
wayne <zhengwei@yunxi.tv>
2020-07-24 17:44:35 +0800

    fix: download

--- book/download.py
+++ book/download.py
@@ -1,25 +1,28 @@
 import requests
 from bs4 import BeautifulSoup
 
+
 def chapter_format(text):
-  return '\n'.join(text.strip().split('\xa0' * 4))
+    return '\n'.join(text.strip().split('\xa0' * 4))
 
-def dowbload_book_chapter(link):
-  resp = requests.get(url = link)
-  resp.encoding = 'utf-8'
-  html = resp.text
-  bs = BeautifulSoup(html, 'lxml')
 
-  # 提取标题和内容
-  book_name = bs.find('title').text
-  text = bs.find('div', id = 'content').text
-  text = chapter_format(text)
+def download_book_chapter(link):
+    resp = requests.get(url=link)
+    resp.encoding = 'utf-8'
+    html = resp.text
+    bs = BeautifulSoup(html, 'lxml')
 
-  with open(book_name + '.txt', 'w') as file:
-    file.write(text)
+    # 提取标题和内容
+    book_name = bs.find('title').text
+    text = bs.find('div', id='content').text
+    text = chapter_format(text)
+
+    with open(book_name + '.txt', 'w') as file:
+        file.write(text)
+
 
 if __name__ == "__main__":
-  # 测试章节下载
-  dowbload_book_chapter(
-    'https://www.xsbiquge.com/15_15338/8549128.html'
-  )
+    # 测试章节下载
+    download_book_chapter(
+        'https://www.xsbiquge.com/15_15338/8549128.html'
+    )
